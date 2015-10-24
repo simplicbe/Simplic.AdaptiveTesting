6182b4c514a0fac606c14b588951ffc19197c0bc
beggers <be@spiegelburg.net>
2015-10-24 17:04:01 +0200

    Add simplic command library

---
+++ src/Simplic.AdaptiveTesting/SAT.Shell/ConsoleHelper/ConsoleHighlight.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAT.Shell
+{
+    /// <summary>
+    /// Create control green highlighting
+    /// </summary>
+    internal class ConsoleSuccess : ConsoleHighlight
+    {
+        /// <summary>
+        /// Passed green to ConsoleHighlight
+        /// </summary>
+        public ConsoleSuccess() : base(ConsoleColor.Green)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Create control warning highlighting
+    /// </summary>
+    internal class ConsoleWarning : ConsoleHighlight
+    {
+        /// <summary>
+        /// Passed yellow to ConsoleHighlight
+        /// </summary>
+        public ConsoleWarning() : base(ConsoleColor.Yellow)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Create control error highlighting
+    /// </summary>
+    internal class ConsoleError : ConsoleHighlight
+    {
+        /// <summary>
+        /// Passed yellow to ConsoleHighlight
+        /// </summary>
+        public ConsoleError() : base(ConsoleColor.Red)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Class for highlighting text in the console output
+    /// </summary>
+    internal class ConsoleHighlight : IDisposable
+    {
+        private ConsoleColor resetColor;
+        
+        /// <summary>
+        /// Create highlight object for using in a using-statement
+        /// </summary>
+        /// <param name="color">New color</param>
+        /// <param name="resetColor">Color for reset on disposing</param>
+        public ConsoleHighlight(ConsoleColor color, ConsoleColor resetColor = ConsoleColor.Gray)
+        {
+            Console.ForegroundColor = color;
+            this.resetColor = resetColor;
+        }
+
+        /// <summary>
+        /// Reset color
+        /// </summary>
+        public void Dispose()
+        {
+            Console.ForegroundColor = resetColor;
+        }
+    }
+}
--- src/Simplic.AdaptiveTesting/SAT.Shell/Program.cs
+++ src/Simplic.AdaptiveTesting/SAT.Shell/Program.cs
@@ -8,8 +8,59 @@ namespace SAT.Shell
 {
     class Program
     {
+        /// <summary>
+        /// Main shell for adpative testing
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            Console.WriteLine("- " + new string ('-', Console.BufferWidth - 3));
+            Console.WriteLine("- Simplic.AdaptiveTesting © EDV-Systeme Spiegelburg GmbH 2015");
+            Console.WriteLine("- Version: 0.1.0.0A");
+            Console.WriteLine("- More information under: https://github.com/simplicbe/Simplic.AdaptiveTesting/");
+            Console.WriteLine("- " + new string('-', Console.BufferWidth - 3));
+
+            // no command
+            if (args.Length == 0)
+            {
+                using (var _waring = new ConsoleWarning())
+                {
+                    Console.WriteLine("No arguments passed.");
+                }
+
+                return;
+            }
+
+            // Create new simplic command shell context for processing the input arguments
+            var context = Simplic.CommandShell.CommandShellManager.Singleton.CreateShellContext("sat");
+
+            // Execute command
+            bool errorOccured = false;
+            string cmd = "";
+
+            foreach (string part in args)
+            {
+                if (cmd != "")
+                {
+                    cmd += " ";
+                }
+
+                cmd += part;
+            }
+
+            string result = context.Execute(cmd, out errorOccured);
+
+            if (errorOccured)
+            {
+                using (var _waring = new ConsoleError())
+                {
+                    Console.WriteLine(result);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result);
+            }
         }
     }
 }
